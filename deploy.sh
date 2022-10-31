127215771584fb06f1f136d703cf83adf39457b7
Vishal Zambare <vishalzambare5@gmail.com>
2022-10-31 19:53:19 +0000

    Update deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 docker build -t vzambare/multi-client:latest -t vzambare/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t vzambare/multi-server:latest -t vzambare/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t vzambare/multi-worker:latest -t vzambare/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
-docker push vzambare/multi-client:$GIT_SHA vzambare/multi-client:latest
-docker push vzambare/multi-server:$GIT_SHA vzambare/multi-server:latest
-docker push vzambare/multi-worker:$GIT_SHA vzambare/multi-worker:latest
+docker push vzambare/multi-client:$GIT_SHA
+docker push vzambare/multi-server:$GIT_SHA
+docker push vzambare/multi-worker:$GIT_SHA
 docker push vzambare/multi-client:latest
 docker push vzambare/multi-server:latest
 docker push vzambare/multi-worker:latest
